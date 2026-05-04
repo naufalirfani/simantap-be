ba9efdf917d226f6f07b3ad3705292910a7f7bdb
Naufal Irfani <naufal.irfani3@gmail.com>
2026-05-04 09:49:49 +0700

    F-07 Halaman Welcome Default Laravel Tampil di Produksi

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Web routes - intentionally empty for production API backend
+// All API routes are defined in routes/api.php
